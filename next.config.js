c88937eb0e40bd18fd47d29198606197db61b9fe
jingruchenmax <jingruchen@outlook.com>
2026-03-20 00:37:57 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,16 @@
  */
 await import("./src/env.js");
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const basePath = isGitHubActions && repoName ? `/${repoName}` : "";
+
 /** @type {import("next").NextConfig} */
 const config = {
 	output: "export",
+	basePath,
+	assetPrefix: basePath || undefined,
+	trailingSlash: true,
 	images: {
 		unoptimized: true,
 	},
